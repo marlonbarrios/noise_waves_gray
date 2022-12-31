1f317da0fa2bc175355b70cfff52480d826e6e4f
marlonbarrios <marlon@dance-tech.net>
2022-12-31 11:45:23 -0500

    fix resizing

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let canvas;
+let p;
+let button;
 
-//Global bars
 let dragging = false;
 let minFrequency = 0.5;
 let maxFrequency = 2;
@@ -8,6 +9,8 @@ let minAmplitude = 0.05;
 let maxAmplitude = 0.5;
 let amplitude;
 let frequency;
+let bgColor;
+
 // Included in index.html
 // This is an alternative to p5.js builtin 'noise' function,
 // It provides 4D noise and returns a value between -1 and 1
@@ -16,10 +19,12 @@ const simplex = new SimplexNoise();
 
 
 function setup() {
+
+  mouseX = width /2;
+  mouseY = height /2;
  
   //template for canvas while printing and exporting/exhition on web/minimal
   canvas = createCanvas(1024, 1024); // will export as 512x512
-
   canvas.style("margin", "auto");
   canvas.style("margin-top", "5%");
   canvas.style("display", "flex");
@@ -31,31 +36,60 @@ function setup() {
   canvas.style("zoom", "0.5");
   canvas.style('dpi', '300');
   canvas.style('bleed', '1/8');
-  noCursor();
-      
-  mouseX = width /2;
-  mouseY = height /2;
-}
+  //noCursor();
 
-function draw() {
- 
-  // clear();
-  background(255);
+//credits
+  createP("'I have waves...'");
+  createP("Move the cursor on the canvas to change the frequency and the amplitude of the waves" );
+  createP("Press 's' to save a png image");
+  var link =createA("https://marlonbarrios.github.io/", "Programmed by Marlon Barrios Solano");
+
+  createElement('title', 'I have waves...')
+
+  var allPs = selectAll("p")
+  for (var i = 0; i < allPs.length; i++) {
+    allPs[i].style("font-family", "Helvetica");
+    allPs[i].style("justify-content", "center");
+    allPs[i].style("align-items", "center");
+    allPs[i].style("position", "relative");
+    allPs[i].style("text-align", "center");
+    allPs[i].style("display", "flex");
+    allPs[i].style("font-size", "15px");
+  }
+
+  link.style("font-family", "Helvetica");
+  link.style("justify-content", "center");
+  link.style("align-items", "center");
+  link.style("position", "relative");
+  link.style("text-align", "center");
+  link.style("display", "flex");
+  link.style("font-size", "15px");
+  link.style("color", "black");
+  link.style("text-decoration", "none");
+
+
 
 
   
-  const frequency = lerp(minFrequency, maxFrequency, mouseX /width);
-  const amplitude = lerp(minAmplitude, maxAmplitude, mouseY /height);
+}
+
+
+
+function draw() {
+  //clear();
+  background(255);
+
+  const frequency = lerp(minFrequency, maxFrequency, mouseX/width );
+  const amplitude = lerp(minAmplitude, maxAmplitude, mouseY/height);
   
   const dim = Math.min(width, height);
   
-
-fill(120);
+noFill();
 stroke(0);
 strokeWeight(dim * 0.0095);
   
   const time = millis()/1000;
-  const rows = 20;
+  const rows = 10;
 
   // Draw each line
   for (let y = 0; y < rows; y++) {
@@ -111,6 +145,8 @@ function drawNoiseLine (opt = {}) {
   endShape();
 }
 
+
+
 function windowResized() {
   const windowWidth = window.innerWidth;
   if (windowWidth < 900) {
@@ -119,17 +155,19 @@ function windowResized() {
     canvas.style("margin-top", "10%");
   
   } else {
-    const windowWidth = window.innerWidth;
-   
       resizeCanvas(windowWidth, windowWidth);
   }
   
 }
 
 function keyPressed() {
-  if (key == 'S' || key == 's') {
-    saveCanvas(canvas, 'Line_waves', 'png');
-  }
+  let m = month();
+  let d = day();
+  let y = year();
+  let t = hour() + ':' + minute();
+  if (key == 'S' || key == 's') 
+    saveCanvas(canvas, 'canvas' + m + d + y + t , 'png');
 }
+  
     
 
